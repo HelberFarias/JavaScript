f7a6c3143f00304e46b9d420472d5b2c15bcbdb0
Helber Farias <helberfarias.ptpro@gmail.com>
2025-05-08 17:05:43 -0300

    Add files via upload

---
+++ Aula 02/ex0.js
@@ -0,0 +1,39 @@
+/*Váriaveis
+let nome = "Helber";
+let idade = 32;
+let altura = 1.67;
+let programador = true;
+
+
+console.log("Nome: ", nome);
+console.log ("Idade: ", idade);
+console.log("Altura:", altura);
+console.log("Programador: ", programador);
+
+Tipos de dados
+console.log("Nome: ", typeof(nome));
+console.log("Idade: ", typeof(idade));
+
+nome.prompt("Qual seu nome? ");
+idade.prompt("Qual a sua idade? ");
+altura.prompt("Qual a sua altura? ");
+programador.prompt("É programador? ")*/
+
+
+let nome;
+let idade;
+let altura;
+let gênero;
+let estudande;
+
+console.log("Nome: ", nome);
+console.log("Idade: ", idade);
+console.log("Altura: ");
+console.log("Gênero: ", gênero);
+console.log("Estudante: ", estudante);
+
+nome.prompt("Qual seu nome? ");
+idade.prompt("Qual a sua idade? ");
+altura.prompt("Qual a sua altura? ");
+gênero.prompt("Qual o seu gênero? ");
+estudante.prompt("É estudante? ");
---
+++ Aula 02/ex01.js
@@ -0,0 +1,17 @@
+let nome;
+let idade;
+let altura;
+let genero;
+let estudante;
+
+nome = prompt("Qual seu nome? ");
+idade = prompt("Qual a sua idade? ");
+altura = prompt("Qual a sua altura? ");
+gênero = prompt("Qual o seu gênero? ");
+estudante = prompt("É estudante? ");
+
+console.log("Nome: ", nome);
+console.log("Idade: ", idade);
+console.log("Altura: ");
+console.log("Gênero: ", gênero);
+console.log("Estudante: ", estudante);
