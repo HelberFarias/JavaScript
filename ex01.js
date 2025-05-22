6b80e1d2c7e7de5212856d0dee8015e3b17d62b8
Helber Farias <helberfarias.ptpro@gmail.com>
2025-05-22 16:48:23 -0300

    Delete ex01.js

--- ex01.js
+++
@@ -1,7 +0,0 @@
-function criarUsuario (nome, idade, email) {
-    this.nome = nome
-    this.idade = idade
-    this.email = email
-}
-const cadastro1 = new criarUsuario ("Helber", 32, "helberjunyo@gmail.com")
-console.log(cadastro1)
